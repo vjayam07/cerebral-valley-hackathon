d8fe21e545660ce6325ffee94b261d65d7ff7f11
vjayam07 <virajjayam@gmail.com>
2025-04-27 06:21:27 -0700

    some edits

---
+++ models/encoder.py
@@ -0,0 +1,10 @@
+#* This model is used to encode our node and edge information each through an independent MLP. 
+
+import torch
+
+
+class NodeEncoder():
+    pass
+
+class EdgeEncoder():
+    pass
